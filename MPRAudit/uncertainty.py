4e9a5c3dff706b0c07226662802f8ac14d218d0b
david-a-siegel <60172187+david-a-siegel@users.noreply.github.com>
2020-01-27 00:39:16 -0800

    Repository for MPRA analysis method "MPRAudit"

---
+++ MPRAudit/uncertainty.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def uncertainty(a,b,da,db):
+    da = np.sqrt(da) #I'm inputting the variance, should be std dev.
+    db = np.sqrt(db) #I'm inputting the variance, shoudl be std dev.
+    return 1-a/b,((b*da)**2+(a*db)**2)/b**4
+
